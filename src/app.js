dcfed69cde0e4340f80b726b9971bc50beefa0d8
alekh-thakur <codewithalekh@gmail.com>
2026-07-06 19:27:03 +0530

    Initialization of Database

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
 const express = require("express")
-
-
+require("dotenv").config();
 const app = express();
+const connectDB = require("./config/database")
+const User = require("./models/User")
+
+app.post("/signup", async (req,res)=>{
+    const user = new User({
+        firstName:"fgdfg",
+        lastName:"fdgdfgl",
+        emailId:"dgfdgdf@example.com",
+        password:"fdgdfl@123",
+        age:45,
+        gender:"Male"
+    })
+
+    try{
+        await user.save();
+        res.send("User data added successfully")
+    }catch(err){
+        res.status(400).send("Error in saving data: " + err.message);
+    }
+})
 
 
-
-const m1 = (req, res, next) => {
-    console.log("M1");
-    next();
-};
-
-const m2 = (req, res, next) => {
-    console.log("M2");
-    next();
-};
-
-const m3 = (req, res, next) => {
-    console.log("M3");
-    next(new Error("end of stack"))
-
-};
-
-const errorHandle = (err,req,res,next)=>{
-    res.status(500).send(err.message)
-}
-
-
-app.get("/", m1, m2, m3);
-app.use(errorHandle)
-
-const PORT =3000
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`);
+connectDB()
+.then(()=>{
+    console.log("Database connected successfully");
+    app.listen(3000,()=>{
+    console.log("Server listening on PORT 3000"); 
+})   
+})
+.catch((err)=>{
+    console.log("Cannot connect to database");
     
 })
+
+
