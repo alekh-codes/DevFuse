c2bf864fc0643fd5fc8c9a882d85bbcb8ed1af42
alekh-thakur <codewithalekh@gmail.com>
2026-07-12 11:01:34 +0530

    Added Login API and Basic Authentication

--- src/app.js
+++ src/app.js
@@ -1,72 +1,85 @@
-const express = require("express")
+const express = require("express");
 require("dotenv").config();
 const app = express();
-const connectDB = require("./config/database")
-const User = require("./models/User")
+const connectDB = require("./config/database");
+const User = require("./models/User");
+const { validateSignup } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 // Add user to database
-app.post("/signup", async (req,res)=>{
-      
-    //Create a new instance of User model
-    const user = new User(req.body)
-    const data = req.body;
+app.post("/signup", async (req, res) => {
+  try {
+    //Validation of data
+    validateSignup(req);
 
+    const { firstName, lastName, emailId, password } = req.body;
+    //Encrypt password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //Create a new instance of User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User data added successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+//Login API
+app.post("/login",async (req,res)=>{
     try{
-        if(data?.firstName.length <3){
-            throw new Error("Name format is invalid")
-        }
-        if(!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(data?.emailId)){
-            throw new Error("Email is invalid")
-        }
-        if(data?.password.length < 8){
-            throw new Error("Password must be of 8 characters")
-        }
-        if(data?.age <18){
-            throw new Error("You are not eligible for this platform")
-        }
-        if(data?.about.length >150){
-            throw new Error("About cannot be more than 10 words")
-        }
-        if(data?.skills.length >10){
-            throw new Error("Skills cannot be more than 10")
-        }
-        await user.save();
-        res.send("User data added successfully")
-    }catch(err){
-        res.status(400).send("Error: " + err.message);
+        const {emailId, password} = req.body;
+
+    const user = await User.findOne({emailId});
+    if(!user){
+        throw new Error("Invalid credentials")
+    }
+    const isValidPassword = await bcrypt.compare(password,user.password);
+    if(isValidPassword){
+        res.send("Login successfull");
+    }
+    else{
+        throw new Error("Invalid credentials")
+    }
+    } catch (err) {
+        res.status(400).send("Invalid credentials");
     }
 })
 
 //Get user by id
-app.get("/user", async (req,res)=>{
-    const id = req.body.id;
-    try{
-        const user = await User.findById(id);
-        if(!user){
-            res.status(404).send("User not found")
-        }
-        else{
-            res.send(user);
-        }
+app.get("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
     }
-    catch(err){
-        res.status(404).send("Somethig went wrong")
-    }
-})
+  } catch (err) {
+    res.status(404).send("Somethig went wrong");
+  }
+});
 
 //Get user by email
-app.get("/user",async (req,res)=>{
-    const email = req.body.emailId;
-    try{
-        const user = await User.findOne({emailId:email})
-        if(!user){
-            res.status(400).send("User not found")   
-        }else{
-            res.send(user);
-        }
-        
+app.get("/user", async (req, res) => {
+  const email = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: email });
+    if (!user) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(user);
+    }
+
     //     const user = await User.find({emailId : email})
     //     if(user.length ===0){
     //         res.status(400).send("User not found");
@@ -74,63 +87,60 @@ app.get("/user",async (req,res)=>{
     //     else{
     //         res.send(user);
     //     }
-    }catch(err){
-        res.status(404).send("Something wnent wrong")
-    }
-})
+  } catch (err) {
+    res.status(404).send("Something wnent wrong");
+  }
+});
 
 //Get all the users
-app.get("/feed",async (req,res)=>{
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }catch(err){
-        res.status(400).send("Something went wrong")
-    }
-})
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 //Delete a user
-app.delete("/user",async (req,res)=>{
-    const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete(userId)
-        res.send("User deleted successfully")
-    }catch(err){
-        res.send("Something went wrong");
-    }
-})
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
 
-app.patch("/user/:userId", async(req,res)=>{
-    const userId = req.params.userId;
-    const data = req.body;
-    try{
-        const AllowedUpdates = ["age","skills","about","gender"];
-        const isUpdateAllowed = Object.keys(data).every(k=>
-            AllowedUpdates.includes(k)
-        )
-        if(!isUpdateAllowed){
-            throw new Error("Update not allowed")
-        }
-        if(data?.skills.length >10){
-            throw new Error("Skills cannot be more than 10")
-        }
-        await User.findByIdAndUpdate(userId,data,{runValidators:true});
-        res.send("User updated successfully!");
-    }catch(err){
-        res.send("Update failed: " + err.message)
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const data = req.body;
+  try {
+    const AllowedUpdates = ["age", "skills", "about", "gender"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      AllowedUpdates.includes(k),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
     }
-})
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    await User.findByIdAndUpdate(userId, data, { runValidators: true });
+    res.send("User updated successfully!");
+  } catch (err) {
+    res.send("Update failed: " + err.message);
+  }
+});
 
 connectDB()
-.then(()=>{
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(3000,()=>{
-    console.log("Server listening on PORT 3000"); 
-})   
-})
-.catch((err)=>{
+    app.listen(3000, () => {
+      console.log("Server listening on PORT 3000");
+    });
+  })
+  .catch((err) => {
     console.log("Cannot connect to database");
-    
-})
-
-
+  });
---
+++ src/utils/validation.js
@@ -0,0 +1,24 @@
+const validator = require("validator")
+const validateSignup = (req) => {
+  const { firstName, emailId, password, age, skills } = req.body;
+  if (firstName.length < 3) {
+    throw new Error("Name format is invalid");
+  }
+  if (!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(emailId)) {
+    throw new Error("Email is invalid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Enter a strong password");
+  }
+  // if (age < 18) {
+  //   throw new Error("You are not eligible for this platform");
+  // }
+  
+  // if (skills.length > 10) {
+  //   throw new Error("Skills cannot be more than 10");
+  // }
+};
+
+module.exports ={
+  validateSignup,
+}
