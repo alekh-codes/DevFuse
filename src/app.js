7803109a66697a5ea3039c745f1c7f3a7ebf995a
alekh <codewithalekh@gmail.com>
2026-08-10 23:13:41 +0530

    Added CORS configurations and updated Image URL

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials:true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,7 @@ const userSchema = new mongoose.Schema({
     },
     imagUrl:{
         type:String,
-        default:"https://www.google.com/imgres?q=user%20image&imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2023%2F02%2F18%2F11%2F00%2Ficon-7797704_640.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Ficon%2520user%2F&docid=dMuvbycXQQi8uM&tbnid=IEMGLHTGr2n8wM&vet=12ahUKEwiMgNLtq8eVAxWV1zgGHQtjCn8QnPAOegUImQEQAA..i&w=640&h=640&hcb=2&ved=2ahUKEwiMgNLtq8eVAxWV1zgGHQtjCn8QnPAOegUImQEQAA",
+        default:"https://portfolio-one-kappa-15.vercel.app/assets/main-DnCC98cz.png",
         validate(value){
             if(!validator.isURL(value)){
                 throw new Error("Invalid Url: " , value)
